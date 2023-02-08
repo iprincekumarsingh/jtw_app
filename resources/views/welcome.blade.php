4dd1aac0855b88f764a08f7f9fcc1db3ccbfb98b
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-02-08 13:42:01 +0530

    added title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Showera</title>
     <link rel="stylesheet" href="{{asset('customcss/new.css')}}">
     <link rel="stylesheet" href="{{asset('style.css')}}">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
